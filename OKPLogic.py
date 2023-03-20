4d8cf3330015adc6deca9431489ff4d053f9e2f4
SweetSub <37567389+tastysugar@users.noreply.github.com>
2023-03-20 10:11:11 -0700

    v0.1.3 Beta
    minor ux improvement

--- OKPLogic.py
+++ OKPLogic.py
@@ -17,7 +17,7 @@ from collections import defaultdict
 import torrent_parser as tp
 from ProcessWindow import MyConsole
 
-VERSION = "v0.1.2 Beta"
+VERSION = "v0.1.3 Beta"
 
 CATEGORY = {
     'Anime': ['Default', 'MV', 'TV', 'Movie', 'Collection', 'Raw', 'English'],
@@ -79,6 +79,7 @@ p, li {{ white-space: pre-wrap; }}
 
         self.reloadTemplate()
         self.updateTemplate()
+        self.selectCookiesChangeHandler(self.menuSelectCookies.currentText())
 
         self.loadProxy()
 
