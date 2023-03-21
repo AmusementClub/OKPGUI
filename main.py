a3961f10c66cd0d8f0ca0ad27a67c4c4f1d4e655
SweetSub <37567389+tastysugar@users.noreply.github.com>
2023-03-20 23:56:07 -0700

    Do not modify style on Windows

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 from OKPLogic import OKPMainWIndow
+import platform
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyle('Windows')
+    if platform.system() != "Windows":
+        app.setStyle('Windows')
 
     window = OKPMainWIndow()
     window.show()
